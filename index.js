89b269c1b9cdfd14fae7c19328147c4d9c7c634d
somrathumayan <somrathumayan@gmail.com>
2025-01-28 22:22:39 +0600

    create a server and send request to browser

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("object");
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello User");
+})
+
+app.listen(8000, () => {
+    console.log("Server is running");
+});

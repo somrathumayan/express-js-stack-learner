f031d780bced94bd5fe9dc2b276f5d0327981f5f
somrathumayan <somrathumayan@gmail.com>
2025-01-28 22:39:08 +0600

    Handle multiple routes

--- index.js
+++ index.js
@@ -3,7 +3,19 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello User");
+    res.send("Hello World");
+})
+
+app.get("/about", (req, res) => {
+    res.send("Hello about");
+})
+
+app.get("/contact", (req, res) => {
+    res.send("Hello contact");
+})
+
+app.get("/blog", (req, res) => {
+    res.send("Hello blog");
 })
 
 app.listen(8000, () => {

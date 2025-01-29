65fb9d929d2774bdcb1157cac81635633530637a
somrathumayan <somrathumayan@gmail.com>
2025-01-29 22:32:54 +0600

    added 4 middleware

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const fs = require("fs");
 const cors = require("cors");
+const morgan = require("morgan");
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(morgan('dev'));
+app.use(cors());
 
 
 app.get("/", (req, res) => {

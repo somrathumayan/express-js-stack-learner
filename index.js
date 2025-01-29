5ee8e0c263fe03b92f41ca313c54a0394666c8db
somrathumayan <somrathumayan@gmail.com>
2025-01-29 22:26:27 +0600

    npm i morgan cors

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const fs = require("fs");
+const cors = require("cors");
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 
 app.get("/", (req, res) => {
     fs.readFile("./pages/index.html", (err, data) => {
